a3f49fcb690a6bdb56462086da6bc00c733fac7b
Luis <soycatorce.developer@gmail.com>
2026-02-13 10:47:53 -0600

    update about me

---
+++ js/about-me.js
@@ -0,0 +1,9 @@
+const img = document.getElementById("about-me--img");
+const h2 = document.getElementById("about-me--h2");
+const p = document.getElementById("about-me--p");
+
+const heightImg = img.clientHeight;
+const heightH2 = h2.clientHeight;
+const heightP = p.clientHeight
+
+2
